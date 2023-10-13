e6afcd53eb0997ab3d4f2844b9c1fb7a49c8e970
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-13 23:44:43 +0300

    lambda tranfering to pq

---
+++ projects/youtube_trending/lambda_wrangling.py
@@ -0,0 +1,43 @@
+import awswrangler as wr # used to access values inside s3 bucket
+import pandas as pd
+import urllib.parse # functions for manipulating and working with urls 
+import os
+
+os_input_s3_cleansed_layer = os.environ['s3_cleansed_layer']
+os_input_glue_catalog_db_name = os.environ['glue_catalog_db_name']
+os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name']
+os_input_write_data_operation = os.environ['write_data_operation']
+
+
+"""
+The  format in which our data is in is user readable json but aws uses parquet 
+strict json so the code below implements that
+We run the code in aws lambda
+"""
+def lambda_handler(event, context):
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+
+        # Creating DF from content
+        df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
+
+        # Extract required columns:
+        df_step_1 = pd.json_normalize(df_raw['items'])
+
+        # Write to S3
+        wr_response = wr.s3.to_parquet(
+            df=df_step_1,
+            path=os_input_s3_cleansed_layer,
+            dataset=True,
+            database=os_input_glue_catalog_db_name,
+            table=os_input_glue_catalog_table_name,
+            mode=os_input_write_data_operation
+        )
+
+        return wr_response
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
