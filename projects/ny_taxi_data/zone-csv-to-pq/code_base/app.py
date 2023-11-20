c77558502d0b8e9c261c51f8133521b94d08a8b5
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-20 15:42:44 +0300

    the transformation application

--- projects/ny_taxi_data/zone-csv-to-pq/code_base/app.py
+++ projects/ny_taxi_data/zone-csv-to-pq/code_base/app.py
@@ -1,13 +1,35 @@
-import json
+import awswrangler as wr
+import pandas as pd
+import urllib.parse
+import os
 
-# import requests
+os_input_s3_cleansed_layer = os.environ['s3_cleansed_layer']
+os_input_glue_catalog_db_name = os.environ['glue_catalog_db_name']
+os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name']
+os_input_write_data_operation = os.environ['write_data_operation']
 
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    # get the bucket and its value
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = urllib.parse.quote_plus(event["Records"][0]["s3"]["object"]["key"], encoding='utf-8')
+
+    try:
+        # create a dataframe
+        df = wr.s3.read_csv(f's3://{bucket}/{key}')
+
+        # write to s3
+        to_s3 = wr.s3.to_parquet(
+            df=df,
+            path=os_input_s3_cleansed_layer,
+            dataset=True,
+            database=os_input_glue_catalog_db_name,
+            table=os_input_glue_catalog_table_name,
+            mode=os_input_write_data_operation
+        )
+        return to_s3
+    except Exception as e:
+        print(e)
+        print(f'Error getting object {key} from bucket {bucket}. Make sure they exist and your bucket is in the same region as this function.')
+        raise e
+
