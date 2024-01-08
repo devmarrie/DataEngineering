627795a73b264a0ab0a7587d60bba14b097a7db9
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-08 14:49:31 +0300

    simple producer that pushes a random data from a csv file

---
+++ projects/stock_market/dags/produce_data.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+import pandas as pd
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from kafka import KafkaProducer
+import json
+
+
+
+default_args = {
+    'owner': 'stocks',
+    'start_date': datetime(2024,1,1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+def produce_stream():
+    df = pd.read_csv('data/TSLA_20-24.csv')
+    record = df.sample(1).to_dict(orient="records")[0]
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send('daily_stock_prices', json.dumps(record).encode('utf-8'))
+
+
+                                                                                                                                                                                                                                                                                                                
+
+with DAG('produce_stock_data',
+         default_args=default_args,
+         schedule='@daily',
+         catchup= False
+) as dag:
+
+    task_produce_stream = PythonOperator(
+        task_id='produce_stream',
+        python_callable=produce_stream
+    )
+
+produce_stream()
