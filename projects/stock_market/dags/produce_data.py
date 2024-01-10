7059046291e1ccc0ed6b2672ddf888e4d76119d7
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-10 17:46:01 +0300

    produce a data sample every minute

--- projects/stock_market/dags/produce_data.py
+++ projects/stock_market/dags/produce_data.py
@@ -4,6 +4,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from kafka import KafkaProducer
 import json
+import time
+import logging
 
 
 
@@ -16,19 +18,25 @@ default_args = {
 
 def produce_stream():
     df = pd.read_csv('data/TSLA_20-24.csv')
-    record = df.sample(1).to_dict(orient="records")[0]
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    producer.send('daily_stock_prices', json.dumps(record).encode('utf-8'))
+    curr_time = time.time()
 
-
-                                                                                                                                                                                                                                                                                                                
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            record = df.sample(1).to_dict(orient="records")[0]
+            producer.send('daily_stock_prices', json.dumps(record).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occoured: {e}')
+            continue
+                                                                                                                                                                                                                                                                                                            
 
 with DAG('produce_stock_data',
          default_args=default_args,
          schedule='@daily',
          catchup= False
-) as dag:
-
+        ) as dag:
     task_produce_stream = PythonOperator(
         task_id='produce_stream',
         python_callable=produce_stream
