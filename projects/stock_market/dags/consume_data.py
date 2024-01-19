e8664fcf2daad198347a8faaaa779e07a9c5da95
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-19 18:24:13 +0300

    just whats needed

--- projects/stock_market/dags/consume_data.py
+++ projects/stock_market/dags/consume_data.py
@@ -7,8 +7,9 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 import os
 
-PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-BIGQUERY_DATASET = os.environ.get("GCP_GCS_DATASET", 'tesla_stocks_dataset')
+PROJECT_ID = os.environ.get('PROJECT_ID')
+# CREDENTIALS_PATH = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+# GCP_CONN_ID = f"google-cloud-platform://?project_id={PROJECT_ID}&keyfile_path={CREDENTIALS_PATH}"
 
 default_args = {
     'owner': 'stocks',
@@ -20,25 +21,21 @@ default_args = {
 # def create_spark_connection():
 
 # def push_data_to_bq():
-def json_read():
-    with open("/.google/credentials/tesla-stocks-410911-c875b633dff5.json", "r") as key_file:
-        key_file_content = key_file.read()
-        print(f"Key File Content: {key_file_content}")
+# def json_read():
+#     with open("/.google/credentials/tesla-stocks-410911-c875b633dff5.json", "r") as key_file:
+#         key_file_content = key_file.read()
+#         print(f"Key File Content: {key_file_content}")
 
 with DAG('consume_stock_data',
          default_args=default_args,
          schedule='@daily',
          catchup= False
         ) as dag:
-    task_contents_read = PythonOperator(
-        task_id="readable_data",
-        python_callable=json_read
-    )
-
     task_create_table = BigQueryCreateEmptyTableOperator(
         task_id="create_table",
         project_id=PROJECT_ID,
-        dataset_id=BIGQUERY_DATASET,
+        dataset_id='tesla_stocks_dataset',
         table_id="test_table"
         )
-    task_create_table
+
+task_create_table
