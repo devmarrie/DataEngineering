ba2a0b8221f7880031625c9d5299a7f717e27ed3
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-06 21:21:58 +0300

    testing each function at a time

--- projects/stock_market/dags/consume_data.py
+++ projects/stock_market/dags/consume_data.py
@@ -4,7 +4,8 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmpt
 from datetime import datetime, timedelta
 
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, DateType, DoubleType, IntegerType
 import logging
 import os
 
@@ -14,13 +15,14 @@ PROJECT_ID = os.environ.get('PROJECT_ID')
 bigquery_dataset = "tesla_stocks_dataset"
 bigquery_table = "twenty_twenty_four_stocks"
 
-def create_spark_connection(s_conn):
+def create_spark_connection():
+    s_conn = None
 
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.google.cloud.spark:spark-bigquery-with-dependencies_2.13:0.36.0," 
-                    "org.apache.spark:spark-streaming_2.13:3.5.0") \
+            .config('spark.jars.packages', "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.36.0," 
+                    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
             .getOrCreate()
 
         s_conn.sparkContext.setLogLevel("ERROR")
@@ -30,27 +32,7 @@ def create_spark_connection(s_conn):
 
     return s_conn
 
-# def create_spark_connection(bigquery_dataset, bigquery_table):
-#     s_conn = None
-
-#     try:
-#         s_conn = SparkSession.builder \
-#             .appName('SparkDataStreaming') \
-#             .config('spark.driver.memory', '8g') \
-#             .config('spark.executor.memory', '4g')  \
-#             .config('spark.sql.warehouse.dir', '/tmp/spark-warehouse') \
-#             .config('bigquery.write.dataset', bigquery_dataset) \
-#             .config('bigquery.write.table', bigquery_table) \
-#             .getOrCreate()
-
-#         s_conn.sparkContext.setLogLevel("ERROR")
-#         logging.info("Spark connection created successfully!")
-#     except Exception as e:
-#         logging.error(f"Couldn't create the spark session due to exception {e}")
-
-#     return s_conn
-
-
+# How do I know its a valid dataframe picked from the topic
 def connect_to_kafka(spark_conn):
     spark_df = None
     try:
@@ -63,26 +45,44 @@ def connect_to_kafka(spark_conn):
         logging.info("kafka dataframe created successfully")
     except Exception as e:
         logging.warning(f"kafka dataframe could not be created because: {e}")
-
     return spark_df
 
-def write_to_bigquery_one(**kwargs):
-    df = kwargs['ti'].xcom_pull(task_ids='connect_to_kafka')
+def create_selection_df_from_kafka(spark_df):
+    schema = StructType([
+        StructField("Date", DateType(), False),
+        StructField("Open", DoubleType(), False),
+        StructField("High", DoubleType(), False),
+        StructField("Low", DoubleType(), False),
+        StructField("Close", DoubleType(), False),
+        StructField("Adj Close", DoubleType(), False),
+        StructField("Volume", IntegerType(), False),
+    ])
+
+    json_df = spark_df.selectExpr("CAST(value AS STRING)") \
+        .select(from_json(col('value'), schema).alias('data')).select("data.*")
+    print(json_df)
+
+    return json_df
+
+def push_to_bigquery(df):
     try:
-        df.write \
-        .outputMode("append") \
-        .format("bigquery") \
-        .option("table", f"{PROJECT_ID}:{bigquery_dataset}.{bigquery_table}") \
-        .start()
+        df.writeStream \
+            .outputMode("append") \
+            .format("bigquery") \
+            .option("table", f"{PROJECT_ID}:{bigquery_dataset}.{bigquery_table}") \
+            .start()
         logging.info("Streaming to BigQuery started successfully")
     except Exception as e:
         logging.warning(f"Failed to start streaming to BigQuery: {e}")
 
 def write_to_bigquery():
-    s_conn = None
-    spark_conn = create_spark_connection(s_conn)
-    df = connect_to_kafka(spark_conn)
-    return df
+    spark_conn = create_spark_connection()
+    if spark_conn is not None:
+        # connect to kafka
+        connect_to_kafka(spark_conn)
+        # sel_df = create_selection_df_from_kafka(df)
+        # push_to_bigquery(df)
+        # return True
 
 default_args = {
     'owner': 'stocks',
@@ -96,27 +96,15 @@ with DAG('consume_stock_data',
          schedule='@daily',
          catchup= False
         ) as dag:
-    task_create_spark_conn = PythonOperator(
-        task_id="create_spark_connection",
-        python_callable=write_to_bigquery
-    )
-
-    task_connect_to_kafka = PythonOperator(
-        task_id="connect_to_kafka",
-        python_callable=connect_to_kafka,
-        provide_context=True
-    )
-
     task_create_table = BigQueryCreateEmptyTableOperator(
         task_id="create_table",
         project_id=PROJECT_ID,
         dataset_id=bigquery_dataset,
         table_id=bigquery_table
     )
-    task_write_to_bigquery = PythonOperator(
-        task_id="write_to_bigquery",
-        python_callable=write_to_bigquery,
-        provide_context=True
+    task_to_bq = PythonOperator(
+        task_id="consume_to_bq",
+        python_callable=write_to_bigquery
     )
 
-task_create_table >> task_create_spark_conn >> task_connect_to_kafka >> task_write_to_bigquery
+task_create_table >> task_to_bq
