8f9d13d5df8399fecf648e0d788a5cb3f938179a
devmarrie <annemarrieakeyo42@gmail.com>
2024-06-04 20:35:45 +0300

    the file must contain parquet

--- projects/carrefour_analysis/load.py
+++ projects/carrefour_analysis/load.py
@@ -12,10 +12,11 @@ def load_to_s3(spark, df, bucket_name, save_path, file_format="parquet", options
     # confgure s3 env
     try:
         avg_discount = df.groupBy('Category').agg({"PercentageDiscount": "avg"}).withColumnRenamed("avg(PercentageDiscount)", "AverageDiscount")
-        avg_discount.write \
-                    .format(file_format) \
-                    .options(**options) \
-                    .save(f"s3a://{bucket_name}/{save_path}")
+        # avg_discount.write \
+        #             .format(file_format) \
+        #             .options(**options) \
+        #             .save(f"s3a://{bucket_name}/{save_path}")
+        avg_discount.write.parquet(f"s3a://{bucket_name}/{save_path}")
         logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
     except Exception as e:
         logging.error(f'Failed to write data to S3: {e}')
@@ -33,11 +34,12 @@ def create_bucket(bucket_name, region):
 
 
 if __name__ == '__main__':
-    jars = ["hadoop-aws-3.4.0.jar", "s3-transfer-manager-2.25.64-javadoc.jar"]         
+    # .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.4.0,com.amazonaws:aws-java-sdk-bundle:1.12.734") \
+    jars = "jar_files/aws-java-sdk-bundle-1.11.1026.jar, jar_files/hadoop-aws-3.3.2.jar"       
     spark = SparkSession.builder \
         .master('local') \
         .appName('c4') \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.4.0,com.amazonaws:aws-java-sdk-bundle:1.12.734") \
+        .config("spark.jars", jars) \
         .getOrCreate()
     
     access_key = os.getenv('AWS_ACCESS_KEY_ID')
@@ -49,11 +51,12 @@ if __name__ == '__main__':
     hadoop_conf.set("com.amazonaws.services.s3.enableV4", "true")
     hadoop_conf.set("fs.s3a.connection.ssl.enabled", "true")
     hadoop_conf.set("fs.s3a.path.style.access", "true")
+    
     df = spark.read.parquet('data/pq/all_foods')
     bucket_name = 'c4-load'
 
     # create_bucket(bucket_name, 'af-south-1')
-    load_to_s3(spark, df, bucket_name, 'avg_discount.parquet')
+    load_to_s3(spark, df, bucket_name, 'avg-file.parquet')
 
     spark.stop()
 
