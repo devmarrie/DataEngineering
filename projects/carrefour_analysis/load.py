64c0c0a81d189117b79bcb1f1253f4306c185a07
devmarrie <annemarrieakeyo42@gmail.com>
2024-06-05 08:21:04 +0300

    loading all the analysed data to s3

--- projects/carrefour_analysis/load.py
+++ projects/carrefour_analysis/load.py
@@ -4,11 +4,13 @@ import os
 from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.functions import min, max, avg, stddev
 
 # load environment variables
 load_dotenv()
 
-def load_to_s3(spark, df, bucket_name, save_path, file_format="parquet", options={}):
+def avg_dis_to_s3(df, bucket_name, save_path, file_format="parquet", options={}):
     # confgure s3 env
     try:
         avg_discount = df.groupBy('Category').agg({"PercentageDiscount": "avg"}).withColumnRenamed("avg(PercentageDiscount)", "AverageDiscount")
@@ -21,7 +23,67 @@ def load_to_s3(spark, df, bucket_name, save_path, file_format="parquet", options
     except Exception as e:
         logging.error(f'Failed to write data to S3: {e}')
         
+# Which products have the highest discounts?
+def highest_dis(df, bucket_name, save_path):
+    try:
+        highest_discounted_product = df.orderBy(col("PercentageDiscount").desc()).limit(10)
+        highest_discounted_product.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
 
+# Which products have the lowest discounts?
+def lowest_dis(df, bucket_name, save_path):
+    try:
+        lowest_discounted_product = df.orderBy(col("PercentageDiscount").asc()).limit(10)
+        lowest_discounted_product.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
+
+# What is the distribution of product prices across categories?
+def price_dist(df, bucket_name, save_path):
+    try:
+        price_distribution = df.groupBy("Category").agg(
+            min("ApplicablePrice").alias("MinPrice"),
+            max("ApplicablePrice").alias("MaxPrice"),
+            avg("ApplicablePrice").alias("AvgPrice"),
+            stddev("ApplicablePrice").alias("StddevPrice")
+        )
+        price_distribution.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
+
+# How many products have a discount applied per category? 
+def dis_products(df, bucket_name, save_path):
+    try:
+        discounted_products_count = df.filter(col("PercentageDiscount") > 0) \
+                                 .groupBy("Category") \
+                                 .count()
+        discounted_products_count.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
+
+# Most expensive products on Carrefour that day
+def top_expensive(df, bucket_name, save_path):
+    try:
+        top_expensive_products = df.orderBy(col("ApplicablePrice").desc()).limit(10)
+        top_expensive_products.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
+
+# 10 cheapest products on Carrefour that day
+def top_cheap(df, bucket_name, save_path):
+    try:
+        top_cheap_products = df.orderBy(col("ApplicablePrice").asc()).limit(10)
+        top_cheap_products.write.parquet(f"s3a://{bucket_name}/{save_path}")
+        logging.info(f'Data successfully written to s3a://{bucket_name}/{save_path}')
+    except Exception as e:
+        logging.error(f'Failed to write data to S3: {e}')
+                      
 def create_bucket(bucket_name, region):
     try:
         s3_client = boto3.client('s3')
@@ -56,8 +118,13 @@ if __name__ == '__main__':
     bucket_name = 'c4-load'
 
     # create_bucket(bucket_name, 'af-south-1')
-    load_to_s3(spark, df, bucket_name, 'avg-file.parquet')
-
+    avg_dis_to_s3(df, bucket_name, 'avg-discount.parquet')
+    highest_dis(df, bucket_name, 'highest_discounted_product.parquet')
+    lowest_dis(df, bucket_name, 'lowest_discounted_product.parquet')
+    price_dist(df, bucket_name, 'price_distribution.parquet')
+    dis_products(df, bucket_name, 'discounted_products_count.parquet')
+    top_expensive(df, bucket_name, 'top_expensive_products.parquet')
+    top_cheap(df, bucket_name, 'top_cheap_products.parquet')
     spark.stop()
 
 
